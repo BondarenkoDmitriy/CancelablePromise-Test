4be8b2614e73a418da36de55ec19f8f026c390fb
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2024-01-17 16:59:47 +0200

    fexed bugs after tests

---
+++ src/cancelable-promise.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class CancelablePromise extends Promise {
+  constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new Error('Executor must be a function');
+    }
+
+    let rejector;
+
+    super((resolve, reject) => {
+      rejector = reject;
+      executor(
+        (value) => {
+          resolve(value);
+        },
+        (reason) => {
+          reject(reason);
+        });
+    });
+
+    this.rejector = rejector; this.isCanceled = false;
+
+    this.cancel = () => {
+      this.isCanceled = true; this.rejector({ isCanceled: this.isCanceled });
+    };
+
+    this.oldThen = this.then;
+
+    this.then = (...args) => {
+      if (args.length > 0 && typeof args[0] !== 'function') {
+        throw new Error();
+      }
+      
+      return this.oldThen(...args);
+    };
+  }
+}
+module.exports = CancelablePromise;
